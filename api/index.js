3e22416151ede70daab549408297b625f54b7a7e
Rehan Ahmad <rehan048686@gmail.com>
2024-08-21 10:11:30 +0500

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from 'express'
-const app = express()
-app.listen(5000 , ()=>{
-    console.log("App is running on port 5000")
-})
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("MongoDB Connected");
+}).catch((error) => {
+  console.log("Error connecting to MongoDB!", error);
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
+
+
